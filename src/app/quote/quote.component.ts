12009fd402e8f1f78743d0a2efe5bb4331cb9f97
Loise Mwarangu <loisemwarangu@gmail.com>
2019-01-11 14:42:10 +0300

    added child component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Quote} from './quote';
+import {Quote} from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
@@ -12,8 +12,7 @@ export class QuoteComponent implements OnInit {
     new Quote (1, "Nara grace", "people who feel the need to control others do not have control over themselves");
     new Quote (2, "shiny  people","if the road that you are on is perfectly clear,then you are probably on someone elses road")
   ]
-
-
+  constructor(){}
   ngOnInit() {
   }
 
