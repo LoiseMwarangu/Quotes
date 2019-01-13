c2d98c2133dd40ef1fb0fc8a33fdfe1751693b93
Loise Mwarangu <loisemwarangu@gmail.com>
2019-01-13 16:43:09 +0300

    added delete button to allow users to delete quotes

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input,Output, EventEmitter } from '@angular/core';
 import {Quote} from '../quote'
 @Component({
   selector: 'app-quote-details',
@@ -6,9 +6,12 @@ import {Quote} from '../quote'
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
-  @Input()quote:Quote;
+  @Input() quote:Quote;
+  @Output() isComplete= new EventEmitter<boolean>();
+  quoteDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
   constructor() { }
-
   ngOnInit() {
   }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,6 +10,15 @@ export class QuoteComponent implements OnInit {
     new Quote(1, 'Nara grace', 'people who feel the need to control others do not have control over themselves', new Date(2018,1,10)),
     new Quote(2, 'shiny  people','if the road that you are on is perfectly clear,then you are probably on someone elses road',new Date(2019,0,1) )
   ]
+  deleteGoal(isComplete,index){
+    if (isComplete){
+        let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index].name}`)
+        
+        if(toDelete){
+            this.quotes.splice(index,1)
+        }
+      }
+    }
   constructor(){}
   ngOnInit() {
   }
